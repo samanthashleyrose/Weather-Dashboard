1a9ff2b4c1d69cdcf9c595ec40022cf2446be1ed
samanthashleyrose <samantha.rose327@gmail.com>
2023-10-26 18:13:45 -0400

    Got search button working - clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,38 @@
 // GLOBAL VARIABLES
 
 const apiKey = 'd90c67317b789f9b8a0455a4ae60d8aa';
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}';
+const apiBaseURL = 'https://api.openweathermap.org/data/2.5/forecast';
 
-const zipcode = document.getElementById
-('zipcode').value;
+const zipcodeInput = document.getElementById
+('zipcode');
 const searchBTN = document.getElementById('search-btn')
 const currentWeatherDisplay = document.getElementById('weather-today')
 const forecastDisplay = document.getElementById('forecast')
 
 // Function to get the API data
-function getAPI() {
-fetch(apiURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function (data) {
-        for (let i = 0; i < data.length; i++) {          
-            const displayWeather = document.createElement('p');
-            displayWeather.textContent = data[i].html_url;
-            forecastDisplay.appendChild(displayWeather);
-        }
-    });
+function getAPI(zipcode) {
+    const apiURL = `${apiBaseURL}?zip=${zipcode}&appid=${apiKey}`;
+
+    fetch(apiURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {          
+                const displayWeather = document.createElement('p');
+                displayWeather.textContent = data[i].html_url;
+                forecastDisplay.appendChild(displayWeather);
+            }
+        });
 };
 
 // Event listener for Search Button
-function handleSearchBTN() {
-    searchBTN.addEventListener('click', getAPI);
-};
+searchBTN.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    console.log('Search Button Clicked');
+    
+    const zipCode = zipcodeInput.value;
+    getAPI(zipCode);
+});
+
 
