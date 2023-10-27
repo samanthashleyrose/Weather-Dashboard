dee7f9acbef2d9b55a2546630731d56dbbc5afa8
samanthashleyrose <samantha.rose327@gmail.com>
2023-10-26 22:50:49 -0400

    Current Weather display now working, data populates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,34 +5,48 @@ const apiBaseURL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 const zipcodeInput = document.getElementById
 ('zipcode');
-const searchBTN = document.getElementById('search-btn')
-const currentWeatherDisplay = document.getElementById('weather-today')
-const forecastDisplay = document.getElementById('forecast')
+const searchBTN = document.getElementById('search-btn');
+const forecastDisplay = document.getElementById('forecast');
+
 
 // Function to get the API data
 function getAPI(zipcode) {
-    const apiURL = `${apiBaseURL}?zip=${zipcode}&appid=${apiKey}`;
+    const apiURL = apiBaseURL+'?zip='+zipcode+'&appid='+apiKey+'&units=imperial';
 
     fetch(apiURL)
         .then(function(response) {
             return response.json();
         })
         .then(function (data) {
-            for (let i = 0; i < data.length; i++) {          
-                const displayWeather = document.createElement('p');
-                displayWeather.textContent = data[i].html_url;
-                forecastDisplay.appendChild(displayWeather);
-            }
-        });
+            console.log(data.city);
+            displayCurrentWeather(data);
+        })
 };
+
+// Function to display Today's Current Weather for entered zipcode
+function displayCurrentWeather(data) {
+    // Data from the API
+    const cityName = data.city.name;
+    const currentDate = new Date();
+    const temperature = data.list[0].main.temp;
+    const windSpeed = data.list[0].wind.speed;
+    const humidity = data.list[0].main.humidity;
+    
+    // Update the current weather display
+    document.getElementById('weather-today').textContent = cityName;
+    document.getElementById('date').textContent = currentDate.toDateString();
+    document.getElementById('temp').textContent = 'Temp: ' + temperature + '°F';
+    document.getElementById('wind').textContent = 'Wind: ' + windSpeed + ' m/s';
+    document.getElementById('humidity').textContent = 'Humidity: ' + humidity + '%';
+}
 
 // Event listener for Search Button
 searchBTN.addEventListener('click', function(event) {
     event.preventDefault(); 
     console.log('Search Button Clicked');
     
-    const zipCode = zipcodeInput.value;
-    getAPI(zipCode);
+    const zipcode = zipcodeInput.value;
+    getAPI(zipcode);
 });
 
 
